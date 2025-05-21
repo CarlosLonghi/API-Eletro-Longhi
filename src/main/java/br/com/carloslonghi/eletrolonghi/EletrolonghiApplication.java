7eb4b6eb8c78e8e1198f2ee43040a11860b08e37
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-20 21:55:35 -0300

    Initial Setup

---
+++ src/main/java/br/com/carloslonghi/eletrolonghi/EletrolonghiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carloslonghi.eletrolonghi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletrolonghiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletrolonghiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carloslonghi/eletrolonghi/EletrolonghiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carloslonghi.eletrolonghi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletrolonghiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
