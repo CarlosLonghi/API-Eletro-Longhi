6a8033b59a5719c10ff2fed3b69571ba6013b70e
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-22 23:01:55 -0300

    refactor: use brand `DTO` in controller and add `ResponseEntity` to responses

--- src/main/java/br/com/carloslonghi/eletrolonghi/controller/BrandController.java
+++ src/main/java/br/com/carloslonghi/eletrolonghi/controller/BrandController.java
@@ -1,12 +1,16 @@
 package br.com.carloslonghi.eletrolonghi.controller;
 
+import br.com.carloslonghi.eletrolonghi.controller.request.BrandRequest;
+import br.com.carloslonghi.eletrolonghi.controller.response.BrandResponse;
 import br.com.carloslonghi.eletrolonghi.entity.Brand;
+import br.com.carloslonghi.eletrolonghi.mapper.BrandMapper;
 import br.com.carloslonghi.eletrolonghi.service.BrandService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/brand")
@@ -16,22 +20,34 @@ public class BrandController {
     private final BrandService brandService;
 
     @GetMapping
-    public List<Brand> getAllBrands() {
-        return brandService.findAll();
+    public ResponseEntity<List<BrandResponse>> getAllBrands() {
+        List<BrandResponse> brands = brandService.findAll()
+                .stream()
+                .map(BrandMapper::toBrandResponse)
+                .toList();
+        return ResponseEntity.ok(brands);
     }
 
     @PostMapping
-    public Brand createBrand(@RequestBody Brand brand) {
-        return brandService.save(brand);
+    public ResponseEntity<BrandResponse> createBrand(@RequestBody BrandRequest request) {
+        Brand newBrand = BrandMapper.toBrandEntity(request);
+        Brand createdBrand = brandService.save(newBrand);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BrandMapper.toBrandResponse(createdBrand));
     }
 
     @GetMapping("/{id}")
-    public Brand getBrandById(@PathVariable Long id) {
-        return brandService.findById(id).orElse(null);
+    public ResponseEntity<BrandResponse> getBrandById(@PathVariable Long id) {
+        return brandService.findById(id)
+                .map(brand -> ResponseEntity.ok(
+                        BrandMapper.toBrandResponse(brand))
+                )
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteBrandById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteBrandById(@PathVariable Long id) {
         brandService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
