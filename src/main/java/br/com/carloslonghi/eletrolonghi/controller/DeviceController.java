346493e8f476d4ecadec835d2f6087aaa71f207f
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-27 19:36:00 -0300

    feat: add `device` service and controller

---
+++ src/main/java/br/com/carloslonghi/eletrolonghi/controller/DeviceController.java
@@ -0,0 +1,67 @@
+package br.com.carloslonghi.eletrolonghi.controller;
+
+import br.com.carloslonghi.eletrolonghi.controller.request.DeviceRequest;
+import br.com.carloslonghi.eletrolonghi.controller.response.DeviceResponse;
+import br.com.carloslonghi.eletrolonghi.entity.Device;
+import br.com.carloslonghi.eletrolonghi.mapper.DeviceMapper;
+import br.com.carloslonghi.eletrolonghi.service.DeviceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/device")
+@RequiredArgsConstructor
+public class DeviceController {
+
+    private final DeviceService deviceService;
+
+    @GetMapping
+    public ResponseEntity<List<DeviceResponse>> getAllDevices() {
+        List<DeviceResponse> devices = deviceService.findAll()
+                .stream()
+                .map(DeviceMapper::toDeviceResponse)
+                .toList();
+
+        return ResponseEntity.ok(devices);
+    }
+
+    @PostMapping
+    public ResponseEntity<DeviceResponse> createDevice(@RequestBody DeviceRequest request) {
+        Device deviceEntity = DeviceMapper.toDeviceEntity(request);
+        Device createdDevice = deviceService.save(deviceEntity);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(DeviceMapper.toDeviceResponse(createdDevice));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DeviceResponse> getDeviceById(@PathVariable Long id) {
+        return deviceService.findById(id)
+                .map(device ->
+                        ResponseEntity.ok(DeviceMapper.toDeviceResponse(device))
+                )
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DeviceResponse> updateDevice(@PathVariable Long id, @RequestBody DeviceRequest request) {
+        Device deviceEntity = DeviceMapper.toDeviceEntity(request);
+
+        return deviceService.update(id, deviceEntity)
+                .map(device ->
+                        ResponseEntity.ok(DeviceMapper.toDeviceResponse(device))
+                )
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDeviceById(@PathVariable Long id) {
+        deviceService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/br/com/carloslonghi/eletrolonghi/service/DeviceService.java
@@ -0,0 +1,80 @@
+package br.com.carloslonghi.eletrolonghi.service;
+
+import br.com.carloslonghi.eletrolonghi.entity.Accessory;
+import br.com.carloslonghi.eletrolonghi.entity.Brand;
+import br.com.carloslonghi.eletrolonghi.entity.Device;
+import br.com.carloslonghi.eletrolonghi.repository.DeviceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DeviceService {
+
+    private final DeviceRepository deviceRepository;
+
+    private final AccessoryService accessoryService;
+    private final BrandService brandService;
+
+    public List<Device> findAll() {
+        return deviceRepository.findAll();
+    }
+
+    public Device save(Device device) {
+        List<Accessory> accessories = this.findAccessories(device.getAccessories());
+        device.setAccessories(accessories);
+
+        Brand brand = this.findBrand(device.getBrand());
+        device.setBrand(brand);
+
+        return deviceRepository.save(device);
+    }
+
+    public Optional<Device> findById(Long id) {
+        return deviceRepository.findById(id);
+    }
+    
+    public Optional<Device> update(Long id, Device device) {
+        Optional<Device> optionalDevice = deviceRepository.findById(id);
+
+        if (optionalDevice.isPresent()) {
+            Brand brand = this.findBrand(device.getBrand());
+            List<Accessory> accessories = this.findAccessories(device.getAccessories());
+
+            Device deviceToUpdate = optionalDevice.get();
+            deviceToUpdate.setModel(device.getModel());
+            deviceToUpdate.setSerialNumber(device.getSerialNumber());
+            deviceToUpdate.setBrand(brand);
+
+            deviceToUpdate.getAccessories().clear();
+            deviceToUpdate.getAccessories().addAll(accessories);
+
+            Device deviceUpdated = deviceRepository.save(deviceToUpdate);
+            return Optional.of(deviceUpdated);
+        }
+
+        return Optional.empty();
+    }
+
+    public void deleteById(Long id) {
+        deviceRepository.deleteById(id);
+    }
+
+    private List<Accessory> findAccessories(List<Accessory> accessories) {
+        List<Accessory> accessoriesFound = new ArrayList<>();
+
+        accessories.forEach(accessory -> {
+            accessoryService.findById(accessory.getId()).ifPresent(accessoriesFound::add);
+        });
+
+        return accessoriesFound;
+    }
+
+    private Brand findBrand(Brand brand) {
+        return brandService.findById(brand.getId()).orElse(null);
+    }
+}
