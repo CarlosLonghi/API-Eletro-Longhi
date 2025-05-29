6600961ec8f60bfc1fb47e5c7a317d165b25ad99
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-28 21:58:59 -0300

    feat: add `customer` entity and repository

---
+++ src/main/java/br/com/carloslonghi/eletrolonghi/entity/Customer.java
@@ -0,0 +1,39 @@
+package br.com.carloslonghi.eletrolonghi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String phone;
+
+    @Column(length = 100)
+    private String email;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
