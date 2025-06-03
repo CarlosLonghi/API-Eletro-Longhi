bf236321cd8560bda403d481b0d7e9649c3534d7
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-06-03 18:57:20 -0300

    feat: add Spring Security `PasswordEncoder` to `UserService`

--- src/main/java/br/com/carloslonghi/eletrolonghi/service/UserService.java
+++ src/main/java/br/com/carloslonghi/eletrolonghi/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.carloslonghi.eletrolonghi.service;
 import br.com.carloslonghi.eletrolonghi.entity.User;
 import br.com.carloslonghi.eletrolonghi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
+
         return userRepository.save(user);
     }
 }
