a8c5ba69586ddc4a3cb7e7c3bea45265a29dd3f7
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-06-03 18:39:01 -0300

    feat: add `AuthService` to implements Spring Security interface `UserDetailsService` and add method `findUserByEmail` to `UserRepository`

--- src/main/java/br/com/carloslonghi/eletrolonghi/repository/UserRepository.java
+++ src/main/java/br/com/carloslonghi/eletrolonghi/repository/UserRepository.java
@@ -2,8 +2,12 @@ package br.com.carloslonghi.eletrolonghi.repository;
 
 import br.com.carloslonghi.eletrolonghi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findUserByEmail(String email);
 }
