660d0be70424944cb3862ca9b6c4ecf13d2511fe
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-06-13 20:57:23 -0300

    feat: add exception `MethodArgumentNotValidException`

--- src/main/java/br/com/carloslonghi/eletrolonghi/config/ApplicationControllerAdvice.java
+++ src/main/java/br/com/carloslonghi/eletrolonghi/config/ApplicationControllerAdvice.java
@@ -2,9 +2,14 @@ package br.com.carloslonghi.eletrolonghi.config;
 
 import br.com.carloslonghi.eletrolonghi.exception.UsernameOrPasswordInvalidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -13,5 +18,16 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNotFoundException(UsernameOrPasswordInvalidException exception) {
         return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handlerArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) ->
+            errors.put(((FieldError)error).getField(), error.getDefaultMessage())
+        );
+
+        return errors;
     }
 }
