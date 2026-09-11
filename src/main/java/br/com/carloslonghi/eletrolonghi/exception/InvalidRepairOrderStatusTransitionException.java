b5de53a8a623d3fec4c73a50faccfa77bdfbee7c
CarlosLonghi <carloslonghi.cl@gmail.com>
2026-09-11 19:37:58 -0300

    fix: usar label em português na mensagem de transição de status inválida
    A exceção interpolava o enum RepairOrderStatus diretamente (toString()),
    expondo o nome bruto da constante (ex: AWAITING_PARTS) no toast de erro
    em vez do label descritivo já usado em outras mensagens do domínio.

--- src/main/java/br/com/carloslonghi/eletrolonghi/exception/InvalidRepairOrderStatusTransitionException.java
+++ src/main/java/br/com/carloslonghi/eletrolonghi/exception/InvalidRepairOrderStatusTransitionException.java
@@ -5,7 +5,7 @@ import br.com.carloslonghi.eletrolonghi.entity.enums.RepairOrderStatus;
 public class InvalidRepairOrderStatusTransitionException extends RuntimeException {
 
     public InvalidRepairOrderStatusTransitionException(RepairOrderStatus current, RepairOrderStatus next) {
-        super("Não é possível mudar o status de " + current + " para " + next + ". " +
+        super("Não é possível mudar o status de " + current.getDescription() + " para " + next.getDescription() + ". " +
               "Só é permitido avançar ou retroceder uma etapa por vez no fluxo.");
     }
 }
